bad4036ff528175fea6bfcc0fa9583169bbdb6d5
Sang <svngdo@gmail.com>
2024-05-17 10:03:53 +0700

    Initial commit

---
+++ src/main/java/com/svngdo/newshubbe/NewsHubBeApplication.java
@@ -0,0 +1,13 @@
+package com.svngdo.newshubbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsHubBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsHubBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/svngdo/newshubbe/NewsHubBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svngdo.newshubbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsHubBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
